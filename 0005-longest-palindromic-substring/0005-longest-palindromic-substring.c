95578f8385152da9437bf2bb8eb0eb0075704205
Mahasvin2007 <m.mahasvinvarma@gmail.com>
2026-09-22 00:32:04 +0530

    Time: 7 ms (64.87%), Space: 8.9 MB (74.68%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.c
@@ -0,0 +1,48 @@
+#include <string.h>
+#include <stdlib.h>
+
+char* longestPalindrome(char* s) {
+    int n = strlen(s);
+
+    if (n == 0) {
+        char *res = (char *)malloc(1);
+        res[0] = '\0';
+        return res;
+    }
+
+    int start = 0;
+    int maxLen = 1;
+
+    for (int i = 0; i < n; i++) {
+
+        // Odd-length palindrome
+        int left = i, right = i;
+        while (left >= 0 && right < n && s[left] == s[right]) {
+            if (right - left + 1 > maxLen) {
+                start = left;
+                maxLen = right - left + 1;
+            }
+            left--;
+            right++;
+        }
+
+        // Even-length palindrome
+        left = i;
+        right = i + 1;
+        while (left >= 0 && right < n && s[left] == s[right]) {
+            if (right - left + 1 > maxLen) {
+                start = left;
+                maxLen = right - left + 1;
+            }
+            left--;
+            right++;
+        }
+    }
+
+    char *result = (char *)malloc((maxLen + 1) * sizeof(char));
+
+    strncpy(result, s + start, maxLen);
+    result[maxLen] = '\0';
+
+    return result;
+}
